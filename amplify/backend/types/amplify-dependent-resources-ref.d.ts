35530673848fc605535a5d763ca429c1cddb5eae
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-15 12:00:43 +0530

    Amplify conf successfully, Removed from .gitignore

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "ZenBuildUp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
