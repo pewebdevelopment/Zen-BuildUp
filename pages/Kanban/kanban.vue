6c17417589fdf23c067d2ac6babefd6f96de0b5e
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-15 11:32:49 +0530

    Assets Images added for Kanban

--- pages/Kanban/kanban.vue
+++ pages/Kanban/kanban.vue
@@ -1,9 +1,12 @@
 <template>
-  <Kanban />
+  <NuxtLayout name="zen">
+    <SectionMain><Kanban /></SectionMain>
+  </NuxtLayout>
 </template>
 
 <script setup>
 import Kanban from "../../components/Kanban/Kanban";
+import SectionMain from "../../components/Sections/SectionMain";
 </script>
 
 <style scoped></style>
