5d322c29acc1fca3277f88f35fc1e8394c05adaf
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-23 23:08:19 +0530

    updates to the perfectly working Kanban

--- pages/Kanban/kanban2.vue
+++ pages/Kanban/kanban2.vue
@@ -7,6 +7,8 @@
 <script setup>
 import Kanban2 from "../../components/Kanban/Kanban2";
 import SectionMain from "../../components/Sections/SectionMain";
+
+// This uses vue 3 Sortable JS (which isnt the official wrapper for Sortable JS)
 </script>
 
 <style scoped></style>
