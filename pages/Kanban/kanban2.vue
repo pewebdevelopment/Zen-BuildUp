51503a4d86722883e27eb17e25a9d6b8b74e2040
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-23 13:41:25 +0530

    Kanban 2 initialized

---
+++ pages/Kanban/kanban2.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout name="zen">
+    <SectionMain><Kanban2 /></SectionMain>
+  </NuxtLayout>
+</template>
+
+<script setup>
+import Kanban2 from "../../components/Kanban/Kanban2";
+import SectionMain from "../../components/Sections/SectionMain";
+</script>
+
+<style scoped></style>
