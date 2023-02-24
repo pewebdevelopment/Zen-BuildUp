b6f8e358f472ee607e23b14493d97a940585f315
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-24 10:51:49 +0530

    AmplifyKanban initialized

---
+++ pages/Kanban/AmplifyKanban.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout name="zen">
+    <AmplifyKanban />
+  </NuxtLayout>
+</template>
+
+<script setup>
+import AmplifyKanban from "../../components/Kanban/AmplifyKanban";
+import SectionMain from "../../components/Sections/SectionMain";
+</script>
+
+<style scoped></style>
