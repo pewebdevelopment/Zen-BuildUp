e89373b77ca89004e00e27e00c8cce3da8af724d
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-25 09:20:36 +0530

    authModeStrategyType:AuthModeStrategyType.MULTI

--- plugins/amplify.ts
+++ plugins/amplify.ts
@@ -1,10 +1,13 @@
 //amplify auth plugin
 import { defineNuxtPlugin } from "#app";
-import { Auth, Amplify } from "aws-amplify";
+import { Amplify, Auth, DataStore, AuthModeStrategyType } from "aws-amplify";
 
 import awsconfig from "@/aws-exports";
 
-Amplify.configure(awsconfig);
+Amplify.configure({
+  ...awsconfig,
+  DataStore: { authModeStrategyType: AuthModeStrategyType.MULTI_AUTH },
+});
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.$auth = Auth;
